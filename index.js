ad9e59f8d8ba018867f92ff97d83ac259380c4ca
Finn <derfinn@gmail.com>
2014-11-02 13:53:23 +0100

    diff test

--- index.js
+++ index.js
@@ -16,24 +16,25 @@ for(lang in langs) {
 glob(htmlGlob, function (err, files) {
   if(err) throw err;
   files.forEach(function (file) {
-    
-    var $i18n = cheerio.load('<span id="i18n" style="display:none; visibility:hidden;"></span>')
 
     console.log(file);
     var $ = cheerio.load(fs.readFileSync(file))
     $('[data-i18n]').each(function (i, el) {
       var key = el.attribs['data-i18n']
       for(lang in langs) {
-        // console.log(lang, langs[lang][key])
-        $i18n('#i18n').append(
-          $i18n('<span>')
+        var value = langs[lang][key]
+        if(!value) {
+          console.error('Missing for', lang, ':', key)
+          continue
+        }
+        $('#i18n').append(
+          $('\n<span>')
             .attr('lang', lang)
             .attr('data-i18n', key)
-            .text(langs[lang][key])
+            .text(value)
         )
       }
     })
-    $('body').append($i18n.html())
     fs.writeFileSync('./test.html', $.html())
     // console.log($i18n.html())
   })
